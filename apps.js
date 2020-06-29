47dd04d56dcb2c5277a1fffd506c61e9f155d171
Ajay Daram <ajayforchrist777@gmail.com>
2020-06-29 21:21:23 +0530

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,12 +1,7 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-
-const btn = document.getElementByID("btn");
+const colors = ["green", "red", "rgba(133,122,200)","#f15025"];
+const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-
-btn.addEventListner("click", function(){
-  const randomNumber = 2;
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
-  
-});
+btn.addEventListener("click", function(){
+  const randomNumber
+})
